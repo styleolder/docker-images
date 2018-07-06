6d42add27e7080b1d2bde5840a6bbad9efa35b08
slpcat <slpcat@qq.com>
2018-07-06 17:46:55 +0800

    qconf

--- qconf/ubuntu16.04-qconf/docker-entrypoint.sh
+++ qconf/ubuntu16.04-qconf/docker-entrypoint.sh
@@ -6,4 +6,4 @@ echo $LOCAL_IDC > conf/localidc
 #set ZOOKEEPER
 sed -i s/^zookeeper.test.*$/zookeeper.${LOCAL_IDC}=${ZK_LIST}/ conf/idc.conf
 #start agent
-bin/agent-cmd.sh start
+bash bin/agent-cmd.sh start
