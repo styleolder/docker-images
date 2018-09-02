e181f85887d0c268e3105a78419707fde30adbdf
root <root@>
2018-09-02 20:35:19 +0800

    rocketmq

--- rocketmq/rocketmq/alpine-rocketmq/rkGenConfig.sh
+++ rocketmq/rocketmq/alpine-rocketmq/rkGenConfig.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 cp /opt/rocketmq/conf/broker.conf.sample /opt/rocketmq/conf/broker.conf
-
 HOST=`hostname -s`
 
 if [[ $HOST =~ (.*)-([0-9]+)$ ]]; then
-    CLUSTER_NAME=${BASH_REMATCH[1]}
-    BROKER_NAME=${BASH_REMATCH[1]}-${BASH_REMATCH[2]}
-    BROKER_ROLE=${BASH_REMATCH[3]}
+    CLUSTER_NAME=$(hostname -s|cut -d - -f -1)
+    BROKER_NAME=$(hostname -s|cut -d - -f -2)-$(hostname -s |awk -F - '{print $NF}')
+    BROKER_ROLE=$(hostname -s|awk -F - '{print $3}')
 else
     echo "Failed to extract ordinal from hostname $HOST"
     exit 1
